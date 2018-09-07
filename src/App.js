2f294b37f0c81d86347f1448b0592d15db91f2c3
fmaldonados <felixmaldonado@unitec.edu>
2018-09-07 00:17:57 -0600

    ahora podes cambiar de color

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import React, {
 } from 'react';
 import './App.css';
 import Canvas from './canvas';
+import { SwatchesPicker } from 'react-color';
 class App extends Component {
+    state = {
+        color: "#0d47a1"
+      }
     render() {
-        return ( <
-            Fragment >
-            <
-            h3 style = {
+        const handleColorChange = ({ hex }) => {
+            this.setState({color:hex});
+        }
+
+        return ( 
+        <Fragment>
+            <h3 style = {
                 {
                     textAlign: 'center'
                 }
-            } > Dos Paint < /h3> <
-            div className = "main" >
-            <
-            div className = "color-guide" >
-            <
-            h5 > Color Guide < /h5> <
-            div className = "user user" > User < /div> <
-            div className = "user guest" > Guest < /div> <
-            /div> <
-            Canvas / >
-            <
-            /div> <
-            /Fragment>
+            } > Dos Paint </h3> <div className = "main" >
+            <div className = "color-guide" >
+                <h5 > Color Guide </h5> 
+                <div className = "user user" > User </div> 
+                <div className = "user guest" > Guest </div> 
+                <SwatchesPicker 
+                 color={this.state.color}
+                onChangeComplete={ handleColorChange } />
+            </div> 
+            <Canvas color={this.state.color}/>
+            </div> 
+        </Fragment>
         );
     }
 }
